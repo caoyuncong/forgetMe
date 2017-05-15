271f7eea6e29ec6d60c9005857fd2d7eb96666c2
Administrator <2780837020@qq.com>
2017-05-15 17:43:05 +0800

    update

--- sql/day1.sql
+++ sql/day1.sql
@@ -1,1 +1,23 @@
 SHOW DATABASES ;
+
+CREATE DATABASE db_1702;
+
+DROP DATABASE db_1702;
+
+USE db_1702;
+
+CREATE TABLE t_student (
+  sno    VARCHAR(10),
+  sname  VARCHAR(10),
+  gender CHAR(1),
+  age    INT(2)
+);
+
+SHOW TABLES ;
+
+SELECT *
+FROM t_student;
+
+INSERT INTO t_student
+VALUES ('2017001', '张三', '男', 12);
+
